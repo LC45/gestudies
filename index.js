62fe7e833402e9e1318be4e583986b62a1acee9d
Lisa Christodoulou <66418330+LC45@users.noreply.github.com>
2020-09-05 19:47:37 +0200

    commit back to top button

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ $(document).ready(function(){
         $("#cookieConsent").fadeOut(200);
     });
 });
+
+$(document).ready(function() {
+$(window).scroll(function() {
+if ($(this).scrollTop() > 20) {
+$('#toTopBtn').fadeIn();
+} else {
+$('#toTopBtn').fadeOut();
+}
+});
+
+$('#toTopBtn').click(function() {
+$("html, body").animate({
+scrollTop: 0
+}, 1000);
+return false;
+});
+});
