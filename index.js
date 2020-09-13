462e68fb01a77715f136256fd6c257ffb19b523c
Lisa Christodoulou <66418330+LC45@users.noreply.github.com>
2020-09-14 01:25:02 +0200

    Commit about pic and text

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-    setTimeout(function () {
-        $("#cookieConsent").fadeIn(200);
-     }, 4000);
-    $("#closeCookieConsent, .cookieConsentOK").click(function() {
-        $("#cookieConsent").fadeOut(200);
-    });
-});
+// $(document).ready(function(){
+//     setTimeout(function () {
+//         $("#cookieConsent").fadeIn(200);
+//      }, 4000);
+//     $("#closeCookieConsent, .cookieConsentOK").click(function() {
+//         $("#cookieConsent").fadeOut(200);
+//     });
+// });
 
 $(document).ready(function() {
 $(window).scroll(function() {
