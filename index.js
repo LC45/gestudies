32fc1f9d944111baca37826f139f749eef4908be
LC45 <lisa@gestudies.com>
2020-10-28 22:31:13 +0100

    still working on cookie banner

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// $(document).ready(function(){
-//     setTimeout(function () {
-//         $("#cookieConsent").fadeIn(200);
-//      }, 4000);
-//     $("#closeCookieConsent, .cookieConsentOK").click(function() {
-//         $("#cookieConsent").fadeOut(200);
-//     });
-// });
+
 
 $(document).ready(function() {
 $(window).scroll(function() {
@@ -23,3 +16,21 @@ scrollTop: 0
 return false;
 });
 });
+
+// <a href="#" onclick="document.cookie='cookiebar=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/'; setupCookieBar(); return false;">Click here to revoke the Cookie consent</a>
+
+
+<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#237afc"
+    },
+    "button": {
+      "background": "#fff",
+      "text": "#237afc"
+    }
+  }
+});
+</script>
