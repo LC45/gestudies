b7f17656cc1f2c3f7404dae2c3ee44d938866660
LC45 <lisa@gestudies.com>
2020-10-29 10:26:06 +0100

    osano script updated to permission

--- index.js
+++ index.js
@@ -16,21 +16,3 @@ scrollTop: 0
 return false;
 });
 });
-
-// <a href="#" onclick="document.cookie='cookiebar=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/'; setupCookieBar(); return false;">Click here to revoke the Cookie consent</a>
-
-
-<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
-<script>
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#237afc"
-    },
-    "button": {
-      "background": "#fff",
-      "text": "#237afc"
-    }
-  }
-});
-</script>
